b700bf8ffc82ca151d1c288d61e5bbdb4e2a5bc2
Kamogelo0404 <kamogelonkadimeng04042gmail.com>
2023-05-23 11:41:27 +0200

    Initial Commit

---
+++ PracticeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeProject
+//
+//  Created by DA MAC M1 156 on 2023/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
