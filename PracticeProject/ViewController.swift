38bd7c3f7b857124d075b50c1fc0d72d9bd80eae
Kamogelo0404 <kamogelonkadimeng04042gmail.com>
2023-05-23 13:09:05 +0200

    IOS

--- PracticeProject/ViewController.swift
+++ PracticeProject/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //the name we used to neam the whole table (tableView)
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,4 +19,14 @@ class ViewController: UIViewController {
 
 
 }
-
+//MARK TableView Methods
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath)
+        
+        return cell
+    }
+}
